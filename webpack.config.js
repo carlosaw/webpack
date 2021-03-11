0ed498eb4949dfe717ba48946b5d5f874731aed0
unknown <carlosfreevg@gmail.com>
2021-03-10 22:10:43 -0400

    Versões

--- webpack.config.js
+++ webpack.config.js
@@ -1,16 +1,19 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry:'./src/index.js',
   output:{
     filename:'script.js',
     path:path.resolve(__dirname, 'dist')
   },
-  mode: 'production',
+  mode:'production',
   module:{
     rules:[
       {test:/\.css$/, use:[ 'style-loader', 'css-loader' ]},
-      {test:/\.(png|jpg|gif|svg)$/, use:[ 'file-loader' ]}           
+      {
+        test: /\.(png|jpg)$/,
+        loader: 'url-loader'
+      }               
     ]
   }
 };
